65e7a1eb7ec940bab428639c6dbd285e87ae4276
Eric Mann <eric@eamann.com>
2012-12-10 11:30:39 -0800

    Initial Structure
    Boilerplate readme and plugin files.

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Plugin Boilerplate
+ * Plugin URI:  https://github.com/ericmann/wp-plugin-boilerplate
+ * Description: WordPress plugin boilerplate framework.
+ * Version:     1.0
+ * Author:      Eric Mann
+ * Author URI:  http://eamann.com
+ * License:     GPLv2+
+ */
